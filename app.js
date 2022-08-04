42fd4b576ae45d51ef2fd77779788f5238c1cb10
Pedro Rosa <pmbr3008@gmail.com>
2022-08-04 16:57:27 +0100

    Fix access test

--- app.js
+++ app.js
@@ -46,11 +46,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.px6oz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.px6oz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 8080);
