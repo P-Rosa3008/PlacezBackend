f86bbac6c40fd43522c123ef35f69d2801b7dd28
Pedro Rosa <pmbr3008@gmail.com>
2022-08-05 01:46:18 +0100

    Test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images")));
+// app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
